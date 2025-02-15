160e02291cb6b0a8f3889b90959e9960d208f5f2
Audrey Ostrom <audreyostrom64@gmail.com>
2025-02-15 15:50:52 -0800

    fix where files are at

--- frontend/app/(tabs)/camera.tsx
+++ frontend/app/(tabs)/camera.tsx
@@ -1,9 +1,137 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { CameraMode, CameraType, CameraView, useCameraPermissions } from "expo-camera";
+import { useRef, useState } from "react";
+import { Alert, Button, Pressable, StyleSheet, Text, View } from "react-native";
+import { Image } from "expo-image";
+import { AntDesign } from "@expo/vector-icons";
+import { Feather } from "@expo/vector-icons";
+import { FontAwesome6 } from "@expo/vector-icons";
+import Constants from "expo-constants";
 
 export default function CameraScreen() {
+  const ref = useRef<CameraView>(null);
+  const [permission, requestPermission] = useCameraPermissions();
+  const [uri, setUri] = useState<string | null>(null);
+  const [mode, setMode] = useState<CameraMode>("picture");
+  const [facing, setFacing] = useState<CameraType>("back");
+  const [recording, setRecording] = useState(false);
+
+  if (!permission) {
+    return null;
+  }
+
+  if (!permission.granted) {
+    return (
+      <View style={styles.container}>
+        <Text style={{ textAlign: "center" }}>
+          We need your permission to use the camera
+        </Text>
+        <Button onPress={requestPermission} title="Grant permission" />
+      </View>
+    );
+  }
+
+  const takePicture = async () => {
+    const photo = await ref.current?.takePictureAsync({ base64: true });
+    if (photo?.base64) {
+      setUri(`data:image/jpeg;base64,${photo.base64}`);
+      uploadImage(photo.base64)
+    }
+  };
+
+  const uploadImage = async (base64Image: string) => {
+    try {
+      const host = Constants.expoConfig?.hostUri?.split(':')[0];
+      const response = await fetch(`http://${host}:9874/upload-image`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ image: base64Image }),
+      });
+
+      const data = await response.json();
+      Alert.alert('Response from Flask:', data.message);
+    } catch (error) {
+      console.error('Error uploading image:', error);
+    }
+  };
+
+  const recordVideo = async () => {
+    if (recording) {
+      setRecording(false);
+      ref.current?.stopRecording();
+      return;
+    }
+    setRecording(true);
+    const video = await ref.current?.recordAsync();
+    console.log({ video });
+  };
+
+  const toggleMode = () => {
+    setMode((prev) => (prev === "picture" ? "video" : "picture"));
+  };
+
+  const toggleFacing = () => {
+    setFacing((prev) => (prev === "back" ? "front" : "back"));
+  };
+
+  const renderPicture = () => {
+    return (
+      <View>
+        <Image
+          source={{ uri }}
+          contentFit="contain"
+          style={{ width: 300, aspectRatio: 1 }}
+        />
+        <Button onPress={() => setUri(null)} title="Take another picture" />
+      </View>
+    );
+  };
+
+  const renderCamera = () => {
+    return (
+      <CameraView
+        style={styles.camera}
+        ref={ref}
+        mode={mode}
+        facing={facing}
+        mute={false}
+        responsiveOrientationWhenOrientationLocked
+      >
+        <View style={styles.shutterContainer}>
+          <Pressable onPress={() => console.log('fix')}>
+            <AntDesign name="picture" size={32} color="white" />
+          </Pressable>
+          <Pressable onPress={takePicture}>
+            {({ pressed }) => (
+              <View
+                style={[
+                  styles.shutterBtn,
+                  {
+                    opacity: pressed ? 0.5 : 1,
+                  },
+                ]}
+              >
+                <View
+                  style={[
+                    styles.shutterBtnInner,
+                    {
+                      backgroundColor: mode === "picture" ? "white" : "red",
+                    },
+                  ]}
+                />
+              </View>
+            )}
+          </Pressable>
+          <Pressable onPress={toggleFacing}>
+            <FontAwesome6 name="rotate-left" size={32} color="white" />
+          </Pressable>
+        </View>
+      </CameraView>
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Camera Screen</Text>
+      {uri ? renderPicture() : renderCamera()}
     </View>
   );
 }
@@ -11,7 +139,37 @@ export default function CameraScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  camera: {
+    flex: 1,
+    width: "100%",
+  },
+  shutterContainer: {
+    position: "absolute",
+    bottom: 44,
+    left: 0,
+    width: "100%",
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: 30,
+  },
+  shutterBtn: {
+    backgroundColor: "transparent",
+    borderWidth: 5,
+    borderColor: "white",
+    width: 85,
+    height: 85,
+    borderRadius: 45,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  shutterBtnInner: {
+    width: 70,
+    height: 70,
+    borderRadius: 50,
   },
 }); 
--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -11,8 +11,6 @@ import { useRouter } from 'expo-router';
 import { useColorScheme } from '@/hooks/useColorScheme';
 import 'global.css';
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
