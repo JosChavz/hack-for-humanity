075d99b5615f18e74bcdb67ec6aa7aa032ad81fb
Audrey Ostrom <audreyostrom64@gmail.com>
2025-02-15 14:46:49 -0800

    add old test endpoint just in case

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
 import base64
+import certifi
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 import os
@@ -7,13 +8,21 @@ import google.generativeai as genai
 import io
 from PIL import Image
 import traceback
+from mongoengine import connect
 
 load_dotenv()
 app = Flask(__name__)
 CORS(app)
 
+connect(host=os.getenv('DATABASE_URI'), ssl=True, tlscafile=certifi.where())
+
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))  # Configure Gemini API key ONCE
 model = genai.GenerativeModel("gemini-2.0-flash")
+
+@app.route('/test-route', methods=['GET'])
+def testRoute():
+    print('hey this is a test')
+    return jsonify({"message": "Test successful!"}), 200
 
 @app.route('/upload-image', methods=['POST'])
 def upload_image():
@@ -27,11 +36,6 @@ def upload_image():
         # Decode base64 string received from the frontend
         image_data = base64.b64decode(base64_image)
         image = Image.open(io.BytesIO(image_data))
-
-        # Prepare image for Gemini (convert to bytes)
-        # buffered = io.BytesIO()
-        # image.save(buffered, format="JPEG")  # Save as JPEG in bytes
-        # image_bytes = buffered.getvalue()
 
         # Prepare Gemini API request (using correct function)
         prompt = "Analyze this image and check if there are any animals. If so, identify the species."
